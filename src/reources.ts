c0c06a5d0d4ef9d36cc1ba9ede2022affc860b3d
sinmu <3038267725@qq.com>
2024-03-27 11:34:25 +0800

    添加几个在线街机游戏网页

--- src/reources.ts
+++ src/reources.ts
@@ -6,6 +6,20 @@ export default {
             desc: "符合直觉的 Vue.js 状态管理库",
             tab: "",
             category: ""
-        }
+        },
+        {
+            src: "https://nes.heheda.top/",
+            name: "红白游戏机盒",
+            desc: "在线畅玩小霸王经典游戏",
+            tab: "",
+            category: ""
+        },
+        {
+            src: "https://mklab.vercel.app/FCGames",
+            name: "FC 游戏在线玩",
+            desc: "FC 游戏在线玩",
+            tab: "",
+            category: ""
+        },
     ]
 }
