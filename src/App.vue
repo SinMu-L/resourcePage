07cdc61a0e1a58d2f8f9f41b9040c3d1e6583bb0
sinmu <3038267725@qq.com>
2024-04-03 18:26:38 +0800

    添加导航栏，先隐藏

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ onBeforeMount(async ()=>{
 
 <template>
 
-  
+  <div class=" sticky top-0 left-0 bg-red-400 w-full h-10 hidden">这里是导航栏</div>
   <div class="grid grid-cols-2 m-1 md:grid-cols-6">
       <Card v-for="(resource,index) in resources" :key="index" :name="resource.name" :src="resource.src" :icon="resource.icon" :desc="resource.desc"></Card>
 
